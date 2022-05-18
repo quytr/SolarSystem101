d5a452ba233130ba113c73090115f06cf5f427df
quytr <quy.truong310@gmail.com>
2022-05-17 20:12:13 -0400

    feature star added but with some error that needs to be resolved

---
+++ src/com/syntax/FeatureStars.java
@@ -0,0 +1,8 @@
+package com.syntax;
+
+public class FeatureStars {
+
+    String size;
+    String color;
+    String radius;
+}
--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,19 +3,24 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         SolarSystem FactorX = new SolarSystem();
-        FactorX.Sun="1";
-        FactorX.Moon="4";
-        FactorX.Planet="2";
-        FactorX.Stars="10000";
+        FactorX.Sun = "1";
+        FactorX.Moon = "4";
+        FactorX.Planet = "2";
+        FactorX.Stars = "10000";
 
         // adding features to our sun
         FeatureSun XFact = new FeatureSun();
-        XFact.color="green";
-        XFact.radius="100000000000km";
-        XFact.heat="43243125423kj";
+        XFact.color = "green";
+        XFact.radius = "100000000000km";
+        XFact.heat = "43243125423kj";
 
+        // adding the features for star in the solar system
+        FeatureStars FacX = new FeatureStars();
+        FacX.color = green;
+        FacX.radius = "3242y563463km";
+        FacX.size = "9999999999m";
     }
 }
